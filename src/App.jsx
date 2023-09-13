9e09b5f0f2f79602f2654d1a85e5b5a824150f2f
Artem <kolyasik124@ukr.net>
2023-09-13 21:18:32 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Navigation from './component/Navigation';
+
+function App() {
+    return (
+        <div className='content'>
+            <Navigation />
+            <hr />
+        </div>
+    );
+}
+
+export default App;
