c6fc0b005a43e5969c1b93a533af213eb33634cb
Artem <kolyasik124@ukr.net>
2023-10-19 20:45:11 +0300

    We need to roll back to JS

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { createRoot } from 'react-dom/client';
 import React from 'react';
 
-import App from './App';
+import App from './App.tsx';
 import './index.scss';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { store } from './redux/store';
